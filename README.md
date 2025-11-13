9e781db18557dd7b1d25d989b1a7276fdb909f4b
DanielMotilla <danielmotillamonreal@outlook.com>
2025-11-12 19:18:00 -0600

    updated project description

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
-# The Triangle Factory
+# The Geometry Factory
 
-A geometry processing library built from scratch in C.
+A geometry processing library that doesn't get in your way.
 
-![The Triangle Factory](images/ok.png)
+No complex dependencies, no undocumented frameworks, no example-only code.
+Just the essentials needed to experiment with geometry, nothing more.
+
+## Vision
+A simple REPL where scientists can test geometric ideas without fighting the tools.
+
+## Current Status
+- OpenGL 3.3 Core visualization
+- CMake build system
+- C17 implementation
+
+## Build
+
+### Dependencies
+- C compiler (GCC or Clang)
+- CMake 3.10 or higher
+- OpenGL development libraries
+- GLFW3
+
+GLAD is included in the source tree. It was generated using the [GLAD web service](https://glad.dav1d.de/) with the following settings:
+- Language: C/C++
+- Specification: OpenGL
+- API: gl Version 3.3
+- Profile: Core
+
+### On Ubuntu/Debian:
+```bash
+# Install dependencies
+sudo apt install build-essential cmake libglfw3-dev libgl1-mesa-dev
+
+# Build
+mkdir build && cd build
+cmake ..
+make
+
+# Run
+./GeometryFactory
+```
+
+### On Other Systems
+Just make sure you have a C compiler, CMake, and GLFW3 installed. The build process is the same.
+
+![The Geometry Factory](images/ok.png)
--- src/main.c
+++ src/main.c
@@ -1,21 +1,24 @@
-// When I started this, I was stupid, now I'm a bit less stupid
-// This is my geometry processing library Tetraktys
-// My goal is simply to learn and maybe make something useful.
+// The Geometry Factory by Daniel Motilla Monreal
+// When I started this, I was stupid; now I'm a bit less stupid
+// Huge thanks to Terry A. Davis for the inspiration to be a better programmer
 
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+// Basic vertex shader that passes through 3D positions
 const char* vertex_shader_src =
  "#version 330 core\n"
  "layout(location = 0) in vec3 aPos;\n"
  "void main() { gl_Position = vec4(aPos, 1.0); }\n";
 
+// Fragment shader that outputs a solid color
 const char* fragment_shader_src =
  "#version 330 core\n"
  "out vec4 FragColor;\n"
  "void main() { FragColor = vec4(1.0, 0.0, 1.0, 1.0); }\n";
 
+// Utility function to compile a shader from source
 GLuint compile_shader(GLenum type, const char* src) {
  GLuint shader = glCreateShader(type);
  glShaderSource(shader, 1, &src, NULL);
@@ -24,6 +27,7 @@ GLuint compile_shader(GLenum type, const char* src) {
 }
 
 int main() {
+    // Initialize GLFW and create a window with OpenGL 3.3 core profile
  glfwInit();
  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -47,12 +51,14 @@ int main() {
  glLinkProgram(program);
  glUseProgram(program);
 
+ // Define a simple triangle in normalized device coordinates
  float verts[] = {
  -0.5f, -0.5f, 0.0f,
  0.5f, -0.5f, 0.0f,
  0.0f, 0.5f, 0.0f
  };
 
+ // Create and setup vertex buffer and vertex array objects
  GLuint vao, vbo;
  glGenVertexArrays(1, &vao);
  glGenBuffers(1, &vbo);
@@ -62,6 +68,7 @@ int main() {
  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
  glEnableVertexAttribArray(0);
 
+ // Main render loop
  while (!glfwWindowShouldClose(window)) {
  glClear(GL_COLOR_BUFFER_BIT);
  glDrawArrays(GL_TRIANGLES, 0, 3);
